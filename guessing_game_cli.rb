831b151615485e0214432105a1e722b0f27a3f38
Phyllis Kan <pyk9q7@gmail.com>
2019-08-09 20:27:18 +0000

    Automatically backed up by Learn

--- guessing_game_cli.rb
+++ guessing_game_cli.rb
@@ -3,11 +3,10 @@ def run_guessing_game
   num = rand(6) + 1
   puts "Please make a guess between 1 to 6!"
   input = gets.chomp
-  while input == "exit"
-    puts "Goodbye!"
-  end
-  if num = input
+  if num == input
     puts "You guessed the correct number!"
+  elsif input == "exit"
+    puts "Goodbye!"
   else
     puts "Sorry! The computer guessed <#{num}>"
   end
