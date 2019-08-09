d486b64470fc89da3da4833968e95f883dd9289e
Phyllis Kan <pyk9q7@gmail.com>
2019-08-09 20:23:23 +0000

    Automatically backed up by Learn

--- guessing_game_cli.rb
+++ guessing_game_cli.rb
@@ -6,7 +6,7 @@ def run_guessing_game
   if input == "exit"
     puts "Goodbye!"
   end
-  if num = input 
+  if num = input
     puts "You guessed the correct number!"
   else
     puts "Sorry! The computer guessed <#{num}>"
